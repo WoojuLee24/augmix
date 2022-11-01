976e7e825ee95a65e6fb496d7af67bcf8f3b58ba
root <woojulee24>
2022-11-01 02:39:46 +0000

    jsdv3.msev1.0, jsdv3.msev1.0.detach initialized

--- cifar.py
+++ cifar.py
@@ -76,7 +76,7 @@ def get_args_from_parser():
                                  'jsdv3.test',
                                  'jsdv3.1', 'jsdv3.1.1',
                                  'jsdv3.log.inv', 'jsdv3.inv',
-                                 'jsdv3.mse',
+                                 'jsdv3.msev1.0', 'jsdv3.msev1.0.detach',
                                  'jsdv3.cossim', 'jsdv3.simsiam', 'jsdv3.simsiamv0.1',
                                  'jsdv3.ntxent', 'jsdv3.ntxent.diff',
                                  'kl',
